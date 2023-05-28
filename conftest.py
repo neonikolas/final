a4bc307611e5ed9ccf5db53bb4b3b24ca29e152b
Neonikolas <191287kzn@mail.ru>
2023-05-28 23:21:32 +0300

    1

---
+++ conftest.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+# This is example shows how we can manage failed tests
+# and make screenshots after any failed test case.
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    # chrome_options.binary_location = '/usr/bin/google-chrome-stable'
+    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--log-level=DEBUG')
+
+    return chrome_options
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    # This function helps to detect that some test failed
+    # and pass this information to teardown:
+
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
